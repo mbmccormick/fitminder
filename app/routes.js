fef4b17293f9d227e6669b513d37fd3efeafd121
Matt McCormick <matt@mbmccormick.com>
2015-03-27 10:54:35 -0700

    Ensuring phone numbers conform to E.164 format.

--- app/routes.js
+++ app/routes.js
@@ -43,7 +43,7 @@ module.exports = function (app, passport) {
             
             // check to see if they are changing their phone number
             if (data.phoneNumber != req.body.phoneNumber) {
-                data.phoneNumber = req.body.phoneNumber;
+                data.phoneNumber = req.body.phoneNumber.startsWith('+1') ? req.body.phoneNumber : '+1' + req.body.phoneNumber;
                 data.isPhoneNumberVerified = false;
                 
                 data.save();
@@ -68,7 +68,7 @@ module.exports = function (app, passport) {
     
     app.post('/api/twilio/inbound', function (req, res) {
     
-        var query = Profile.where({ phoneNumber: req.body.From });
+        var query = Profile.where({ phoneNumber: (req.body.From.startsWith('+1') ? req.body.From : '+1' + req.body.From) });
 
         // find the user profile associated with this phone number
         query.findOne(function (err, data) {
